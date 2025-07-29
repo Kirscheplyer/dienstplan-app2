66bbe7f3b48d23b50a4135909fc5f7e994a3764d
Kirscheplyer <141914577+Kirscheplyer@users.noreply.github.com>
2025-07-29 22:32:12 +0200

    Add files via upload

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,6 +5,7 @@ import DienstplanApp from "../DienstplanApp";
 
 const ADMIN_ID = "user_30NpYU323qGA3LO4JedrBWRQXXP";
 
+const gespeicherteMitarbeiter = JSON.parse(localStorage.getItem("mitarbeiterListe")) || [];
 const mitarbeiterListe = [
   {
     name: "Aya",
@@ -40,6 +41,9 @@ const mitarbeiterListe = [
   { name: "Andre", rolle: "ZFA" },
   { name: "Susanne", rolle: "ZFA" },
 ];
+
+// lokale Ergänzungen hinzufügen
+mitarbeiterListe.push(...gespeicherteMitarbeiter.filter(m => !mitarbeiterListe.some(e => e.name === m.name)));
 
 const schichtzeiten = {
   moDo: {
